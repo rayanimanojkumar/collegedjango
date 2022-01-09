b3d81270f78eb922bd9c1a6fbdbb491e23067770
rayanimanojkumar <84898769+rayanimanojkumar@users.noreply.github.com>
2022-01-09 08:40:01 +0530

    Update urls.py

--- MYSITE/college/urls.py
+++ MYSITE/college/urls.py
@@ -5,7 +5,7 @@ from .import views
 # from rest_framework_swagger.views import get_swagger_view
 # from rest_framework.schemas import get_schema_view
 
-
+app_name='student_api'
 
 # schema_view = get_swagger_view(title='Student API')
 #
@@ -22,7 +22,9 @@ urlpatterns=[
            # path('student',schema_view),
            path('marks', views.MarksView.as_view(), name='marks'),
            path('student',views.StudentView.as_view(), name='student_details')
-
+           path('student_details',views.StudentDetailsView, name='student_info'),
+           path('teacherslist', views.ListCreateTeacher.as_view(), name='teacherlist'),
+#            path('auth',include('rest_framework.urls'), name='rest_framework'),
 
 ]
 
