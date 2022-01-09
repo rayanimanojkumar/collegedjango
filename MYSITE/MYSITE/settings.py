1adf2db743f9a3ac31361a72d60f90001d09717a
rayanimanojkumar <84898769+rayanimanojkumar@users.noreply.github.com>
2022-01-09 08:34:41 +0530

    Update settings.py

--- MYSITE/MYSITE/settings.py
+++ MYSITE/MYSITE/settings.py
@@ -74,6 +74,13 @@ TEMPLATES = [
 
 REST_FRAMEWORK = {
     'DEFAULT_SCHEMA_CLASS': 'rest_framework.schemas.coreapi.AutoSchema'
+#     'DEFAULT_AUTHENTICATION_CLASSES': [
+#         'rest_framework.authentication.BasicAuthentication',
+
+#         # 'rest_framework.authentication.SessionAuthentication',
+#     ],
+#    'DEFAULT_PERMISSION_CLASSES':['rest_framework.permissions.IsAuthenticated']
+
 }
 
 
